8a1ed986abc7035b682489b5c304f4e169e7e165
hheeju98 <hheeju98@naver.com>
2024-08-15 18:45:04 +0900

    Object.create

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // 생성자는 대문자로 시작
 const Person = function (firstName, birthYear) {
     // Instance properties
@@ -190,3 +191,27 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+*/
+
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+}
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
